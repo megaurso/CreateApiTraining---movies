b8e722784bc2b3c88d88cff33a2d89cda01946cd
megaurso <bernadogui@hotmail.com>
2023-02-06 14:39:39 -0300

    entrega completa

--- createTables.sql
+++ createTables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE listmovies;
 
 CREATE TABLE IF NOT EXISTS movies (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT UNIQUE NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
     description TEXT,
     duration INT NOT NULL,
     price INT NOT NULL
